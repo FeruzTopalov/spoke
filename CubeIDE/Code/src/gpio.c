e899ca3a461518170d70d086b4abb2db2c01757e
Feruz Topalov <fzzavod@yandex.ru>
2023-04-14 21:58:18 +0200

    Update gpio_init()

--- CubeIDE/Code/src/gpio.c
+++ CubeIDE/Code/src/gpio.c
@@ -18,63 +18,76 @@ void gpio_init(void)
     //Port A
     RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
     
-    //PA0 - ADC0 (Battery voltage)
-    GPIOA->CRL &= ~GPIO_CRL_MODE0;      //input mode
-    GPIOA->CRL &= ~GPIO_CRL_CNF0;       //analog input
+    //PA0 - Piezo Buzzer (PWM) P
+    GPIOA->CRL &= ~GPIO_CRL_MODE0_0;    //output 2 MHz
+    GPIOA->CRL |= GPIO_CRL_MODE0_1;
+    GPIOA->CRL &= ~GPIO_CRL_CNF0;       //alternate output push-pull
+    GPIOA->CRL |= GPIO_CRL_CNF0_1;
     
-    //PA1 - Button UP
-    GPIOA->CRL &= ~GPIO_CRL_MODE1;      //input mode
-    GPIOA->CRL &= ~GPIO_CRL_CNF1_0;     //input with pull
-    GPIOA->CRL |= GPIO_CRL_CNF1_1; 
-    GPIOA->ODR |= GPIO_ODR_ODR1;        //pull-up on
+    //PA1 - Piezo Buzzer (PWM) N
+    GPIOA->CRL &= ~GPIO_CRL_MODE1_0;  	//output 2 MHz
+    GPIOA->CRL |= GPIO_CRL_MODE1_1;
+    GPIOA->CRL &= ~GPIO_CRL_CNF1_0;    	//alternate output push-pull
+    GPIOA->CRL |= GPIO_CRL_CNF1_1;
     
-    //PA2 - Button DOWN
+    //PA2 - ADC2 (Battery voltage)
     GPIOA->CRL &= ~GPIO_CRL_MODE2;      //input mode
-    GPIOA->CRL &= ~GPIO_CRL_CNF2_0;     //input with pull
-    GPIOA->CRL |= GPIO_CRL_CNF2_1; 
-    GPIOA->ODR |= GPIO_ODR_ODR2;        //pull-up on
+    GPIOA->CRL &= ~GPIO_CRL_CNF2;       //analog input
     
-    //PA3 - Button OK
-    GPIOA->CRL &= ~GPIO_CRL_MODE3;      //input mode
-    GPIOA->CRL &= ~GPIO_CRL_CNF3_0;     //input with pull
-    GPIOA->CRL |= GPIO_CRL_CNF3_1;
-    GPIOA->ODR |= GPIO_ODR_ODR3;        //pull-up on
+    //PA3 - RF CS
+    GPIOA->CRL &= ~GPIO_CRL_MODE3_0;   //output 2 MHz
+    GPIOA->CRL |= GPIO_CRL_MODE3_1;
+    GPIOA->CRL &= ~GPIO_CRL_CNF3;      //output push-pull
     
-    //PA6 - Piezo Buzzer (PWM) P
-    GPIOA->CRL &= ~GPIO_CRL_MODE6_0;    //output 2 MHz
-    GPIOA->CRL |= GPIO_CRL_MODE6_1;
-    GPIOA->CRL &= ~GPIO_CRL_CNF6;       //alternate output push-pull
-    GPIOA->CRL |= GPIO_CRL_CNF6_1;
+    //PA4 - RF Reset
+    GPIOA->CRL &= ~GPIO_CRL_MODE4_0;   //output 2 MHz
+    GPIOA->CRL |= GPIO_CRL_MODE4_1;
+    GPIOA->CRL &= ~GPIO_CRL_CNF4;      //output push-pull
 
-    //PA7 - Piezo Buzzer (PWM) N
-    GPIOA->CRL &= ~GPIO_CRL_MODE7_0;  	//output 2 MHz
+    //PA5 - RF SCK
+    GPIOA->CRL &= ~GPIO_CRL_MODE5_0;    //output 2 MHz
+    GPIOA->CRL |= GPIO_CRL_MODE5_1;
+    GPIOA->CRL &= ~GPIO_CRL_CNF5_0;     //alternate output push-pull
+    GPIOA->CRL |= GPIO_CRL_CNF5_1;
+
+    //PA6 - RF MISO
+    GPIOA->CRL &= ~GPIO_CRL_MODE6;      //input mode
+    GPIOA->CRL |= GPIO_CRL_CNF6_0;      //floating input
+    GPIOA->CRL &= ~GPIO_CRL_CNF6_1;
+
+    //PA7 - RF MOSI
+    GPIOA->CRL &= ~GPIO_CRL_MODE7_0;    //output 2 MHz
     GPIOA->CRL |= GPIO_CRL_MODE7_1;
-    GPIOA->CRL &= ~GPIO_CRL_CNF7_0;    	//alternate output push-pull
+    GPIOA->CRL &= ~GPIO_CRL_CNF7_0;     //alternate output push-pull
     GPIOA->CRL |= GPIO_CRL_CNF7_1;
 
-    //PA8 - RES (SSD1306)
-    GPIOA->CRH &= ~GPIO_CRH_MODE8_0;    //output 2 MHz
+    //PA8 - LCD D/C
+    GPIOA->CRH &= ~GPIO_CRH_MODE8_0;   //output 2 MHz
     GPIOA->CRH |= GPIO_CRH_MODE8_1;
-    GPIOA->CRH &= ~GPIO_CRH_CNF8;      	//output push-pull
+    GPIOA->CRH &= ~GPIO_CRH_CNF8;      //output push-pull
     
-    //PA9 - USART TX1 (GPS config)
+    //PA9 - USART1 TX (Console)
     GPIOA->CRH &= ~GPIO_CRH_MODE9_0;    //output 2 MHz
     GPIOA->CRH |= GPIO_CRH_MODE9_1;
     GPIOA->CRH &= ~GPIO_CRH_CNF9_0;     //alternate output push-pull
     GPIOA->CRH |= GPIO_CRH_CNF9_1;
 
-    //PA10 - USART RX1 (GPS NMEA stream)
+    //PA10 - USART1 RX (Console)
     GPIOA->CRH &= ~GPIO_CRH_MODE10;     //input
     GPIOA->CRH |= GPIO_CRH_CNF10_0;     //alternate input floating
     GPIOA->CRH &= ~GPIO_CRH_CNF10_1;
 
-    //PA11 - GPS (PPS interrupt)
-    GPIOA->CRH &= ~GPIO_CRH_MODE11;     //input mode
-    GPIOA->CRH &= ~GPIO_CRH_CNF11_0;    //input with pull
-    GPIOA->CRH |= GPIO_CRH_CNF11_1;
-    GPIOA->ODR &= ~GPIO_ODR_ODR11;      //pull-down
+    //PA11 - RF TX Enable
+    GPIOA->CRH &= ~GPIO_CRH_MODE11_0;   //output 2 MHz
+    GPIOA->CRH |= GPIO_CRH_MODE11_1;
+    GPIOA->CRH &= ~GPIO_CRH_CNF11;      //output push-pull
     
-    //PA15 - RF Chip Select
+    //PA12 - RF RX Enable
+    GPIOA->CRH &= ~GPIO_CRH_MODE12_0;   //output 2 MHz
+    GPIOA->CRH |= GPIO_CRH_MODE12_1;
+    GPIOA->CRH &= ~GPIO_CRH_CNF12;      //output push-pull
+
+    //PA15 - Power Switch Hold
     GPIOA->CRH &= ~GPIO_CRH_MODE15_0;   //output 2 MHz
     GPIOA->CRH |= GPIO_CRH_MODE15_1;
     GPIOA->CRH &= ~GPIO_CRH_CNF15;      //output push-pull
@@ -82,61 +95,90 @@ void gpio_init(void)
     //Port B
     RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
     
-    //PB0 - Green LED
-    GPIOB->CRL &= ~GPIO_CRL_MODE0_0;    //output 2 MHz
-    GPIOB->CRL |= GPIO_CRL_MODE0_1;
-    GPIOB->CRL &= ~GPIO_CRL_CNF0;       //output push-pull
+    //PB0 - RF IRQ
+    GPIOB->CRL &= ~GPIO_CRL_MODE0;      //input mode
+    GPIOB->CRL &= ~GPIO_CRL_CNF0_0;     //input with pull
+    GPIOB->CRL |= GPIO_CRL_CNF0_1;
+    GPIOB->ODR |= GPIO_ODR_ODR0;        //pull-up
     
-    //PB1 - Red LED
-    GPIOB->CRL &= ~GPIO_CRL_MODE1_0;    //output 2 MHz
-    GPIOB->CRL |= GPIO_CRL_MODE1_1;
-    GPIOB->CRL &= ~GPIO_CRL_CNF1;       //output push-pull
+    //PB1 - RF Busy ??? pull-up
+    GPIOB->CRL &= ~GPIO_CRL_MODE1;      //input mode
+    GPIOB->CRL |= GPIO_CRL_CNF1_0;      //floating input
+    GPIOB->CRL &= ~GPIO_CRL_CNF1_1;
 
-    //PB3 - RF Clock
-    GPIOB->CRL &= ~GPIO_CRL_MODE3_0;    //output 2 MHz
-    GPIOB->CRL |= GPIO_CRL_MODE3_1;
-    GPIOB->CRL &= ~GPIO_CRL_CNF3_0;     //alternate output push-pull
+    //PB2 - GPS (PPS interrupt)
+    GPIOB->CRL &= ~GPIO_CRL_MODE2;     //input mode
+    GPIOB->CRL &= ~GPIO_CRL_CNF2_0;    //input with pull
+    GPIOB->CRL |= GPIO_CRL_CNF2_1;
+    GPIOB->ODR &= ~GPIO_ODR_ODR2;      //pull-down
+
+    //PB3 - Button ESC
+    GPIOB->CRL &= ~GPIO_CRL_MODE3;      //input mode
+    GPIOB->CRL &= ~GPIO_CRL_CNF3_0;     //input with pull
     GPIOB->CRL |= GPIO_CRL_CNF3_1;
+    GPIOB->ODR |= GPIO_ODR_ODR3;        //pull-up on
 
-    //PB4 - RF MISO
+    //PB4 - Button OK
     GPIOB->CRL &= ~GPIO_CRL_MODE4;      //input mode
-    GPIOB->CRL |= GPIO_CRL_CNF4_0;      //floating input
-    GPIOB->CRL &= ~GPIO_CRL_CNF4_1;
+    GPIOB->CRL &= ~GPIO_CRL_CNF4_0;     //input with pull
+    GPIOB->CRL |= GPIO_CRL_CNF4_1;
+    GPIOB->ODR |= GPIO_ODR_ODR4;        //pull-up on
 
-    //PB5 - RF MOSI
-    GPIOB->CRL &= ~GPIO_CRL_MODE5_0;    //output 2 MHz
-    GPIOB->CRL |= GPIO_CRL_MODE5_1;
-    GPIOB->CRL &= ~GPIO_CRL_CNF5_0;     //alternate output push-pull
+    //PB5 - Button PWR
+    GPIOB->CRL &= ~GPIO_CRL_MODE5;      //input mode
+    GPIOB->CRL &= ~GPIO_CRL_CNF5_0;     //input with pull
     GPIOB->CRL |= GPIO_CRL_CNF5_1;
+    GPIOB->ODR |= GPIO_ODR_ODR5;        //pull-up on
 
-    //PB8 - RF IRQ0
-    GPIOB->CRH &= ~GPIO_CRH_MODE8;      //input mode
-    GPIOB->CRH &= ~GPIO_CRH_CNF8_0;     //input with pull
-    GPIOB->CRH |= GPIO_CRH_CNF8_1;
-    GPIOB->ODR |= GPIO_ODR_ODR8;        //pull-up
+    //PB6 - ACC/MAG SCL
+    GPIOB->CRL &= ~GPIO_CRL_MODE6_0;   //output 2 MHz
+    GPIOB->CRL |= GPIO_CRL_MODE6_1;
+    GPIOB->CRL |= GPIO_CRL_CNF6;       //alternate function open-drain
 
-    //PB9 - Power Switch
-    GPIOB->CRH &= ~GPIO_CRH_MODE9_0;   	//output 2 MHz
-    GPIOB->CRH |= GPIO_CRH_MODE9_1;
-    GPIOB->CRH &= ~GPIO_CRH_CNF9;		//output push-pull
+    //PB7 - ACC/MAG SCL
+    GPIOB->CRL &= ~GPIO_CRL_MODE7_0;   //output 2 MHz
+    GPIOB->CRL |= GPIO_CRL_MODE7_1;
+    GPIOB->CRL |= GPIO_CRL_CNF7;       //alternate function open-drain
+
+    //PB8 - Red LED
+    GPIOB->CRH &= ~GPIO_CRH_MODE8_0;    //output 2 MHz
+    GPIOB->CRH |= GPIO_CRH_MODE8_1;
+    GPIOB->CRH &= ~GPIO_CRH_CNF8;       //output push-pull
+
+    //PB9 - Green LED
+    GPIOB->CRH &= ~GPIO_CRH_MODE8_0;    //output 2 MHz
+    GPIOB->CRH |= GPIO_CRH_MODE8_1;
+    GPIOB->CRH &= ~GPIO_CRH_CNF8;       //output push-pull
     
-    //PB12 - CS (SSD1306)
+    //PB10 - USART3 TX (GPS)
+    GPIOB->CRH &= ~GPIO_CRH_MODE10_0;    //output 2 MHz
+    GPIOB->CRH |= GPIO_CRH_MODE10_1;
+    GPIOB->CRH &= ~GPIO_CRH_CNF10_0;     //alternate output push-pull
+    GPIOB->CRH |= GPIO_CRH_CNF10_1;
+
+    //PB11 - USART3 RX (GPS)
+    GPIOB->CRH &= ~GPIO_CRH_MODE11_0;    //output 2 MHz
+    GPIOB->CRH |= GPIO_CRH_MODE11_1;
+    GPIOB->CRH &= ~GPIO_CRH_CNF11_0;     //alternate output push-pull
+    GPIOB->CRH |= GPIO_CRH_CNF11_1;
+
+    //PB12 - LCD CS
     GPIOB->CRH &= ~GPIO_CRH_MODE12_0;   //output 2 MHz
     GPIOB->CRH |= GPIO_CRH_MODE12_1;
     GPIOB->CRH &= ~GPIO_CRH_CNF12;      //output push-pull
     
-    //PB13 - SCK (SSD1306)
+    //PB13 - LCD SCK
     GPIOB->CRH &= ~GPIO_CRH_MODE13_0;   //output 2 MHz
     GPIOB->CRH |= GPIO_CRH_MODE13_1;
     GPIOB->CRH &= ~GPIO_CRH_CNF13_0;    //alternate output push-pull
     GPIOB->CRH |= GPIO_CRH_CNF13_1;
     
-    //PB14 - D/C (SSD1306)
+    //PB14 - LCD Reset
     GPIOB->CRH &= ~GPIO_CRH_MODE14_0;   //output 2 MHz
     GPIOB->CRH |= GPIO_CRH_MODE14_1;
     GPIOB->CRH &= ~GPIO_CRH_CNF14;      //output push-pull
     
-    //PB15 - MOSI (SSD1306)
+    //PB15 - LCD MOSI
     GPIOB->CRH &= ~GPIO_CRH_MODE15_0;   //output 2 MHz
     GPIOB->CRH |= GPIO_CRH_MODE15_1;
     GPIOB->CRH &= ~GPIO_CRH_CNF15_0;    //alternate output push-pull
@@ -145,10 +187,21 @@ void gpio_init(void)
     //Port C
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
 
-    //PC13 - X6 test point
-    GPIOC->CRH &= ~GPIO_CRH_MODE13_0;    //output 2 MHz
-    GPIOC->CRH |= GPIO_CRH_MODE13_1;
-    GPIOC->CRH &= ~GPIO_CRH_CNF13;       //output push-pull
+    //PC13 - ACC/MAG IRQ
+    GPIOC->CRH &= ~GPIO_CRH_MODE13;      //input mode
+    GPIOC->CRH &= ~GPIO_CRH_CNF13_0;     //input with pull
+    GPIOC->CRH |= GPIO_CRH_CNF13_1;
+    GPIOC->ODR |= GPIO_ODR_ODR13;        //pull-up
+
+    //PC14 - X4 test point
+    GPIOC->CRH &= ~GPIO_CRH_MODE14_0;    //output 2 MHz
+    GPIOC->CRH |= GPIO_CRH_MODE14_1;
+    GPIOC->CRH &= ~GPIO_CRH_CNF14;       //output push-pull
+
+    //PC15 - X5 test point
+    GPIOC->CRH &= ~GPIO_CRH_MODE15_0;    //output 2 MHz
+    GPIOC->CRH |= GPIO_CRH_MODE15_1;
+    GPIOC->CRH &= ~GPIO_CRH_CNF15;       //output push-pull
 }
 
 
